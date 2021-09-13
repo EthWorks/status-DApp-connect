3926b451a0dc0eb7f44d5f5073674f0852a4dc82
Pawel <Pawel@Tests-MacBook-Air.local>
2021-09-13 15:50:01 +0200

    Grid sections

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,71 @@ module.exports = {
 	purge: ["./src/**/*.css", "./**/*.html"],
 	darkMode: false,
 	theme: {
-		extend: {},
+		colors: {
+			rectangle96: '#1746CA',
+			rectangle97: '#132644',
+			rectangle98: '#8BC3DE',
+			rectangle99: '#E1FF67',
+			rectangle101: '#242424',
+			rectangle102: '#fff',
+			blue: '#3A71F9',
+			grey: '#D5D5D5',
+			green: '#60D086',
+		},
+		container: {
+			padding: {
+				DEFAULT: '1.5rem',
+				md: '2rem',
+			},
+		},
+		fontFamily: {
+			'body': ['"Helvetica"'],
+			'special': ['"Open Sans"']
+		},
+		screens: {
+			'none': '100%',
+			'sm': '640px',
+			'md': '768px',
+			'lg': '1024px',
+			'xl': '1340px',
+			'xxl': '1404px',
+			'xxxl': '1700px'
+		},
+		extend: {
+			backgroundImage: theme => ({
+				'link-arrow-black': "url('../images/arrow/arrow-black.svg')",
+				'link-arrow-blue': "url('../images/arrow/arrow-blue.svg')",
+				'link-arrow-green': "url('../images/arrow/arrow-green.svg')",
+			}),
+			fontFamily: {
+				'sans': 'Helvetica'
+			},
+			fontSize: {
+				title: ['32px', '38px'],
+				'3.5xl': ['40px', '48px'],
+			},
+			gap: theme => ({
+				'22': '5.5rem',
+			}),
+			height: theme => ({
+				'58': '14.5rem',
+			}),
+			maxWidth: theme => ({
+				'31': '31rem',
+				'code': '38.375rem'
+			}),
+			padding: theme => ({
+				'25': '6.25rem',
+				'26': '6.5rem',
+				'35': '8.75rem',
+			}),
+			margin: theme => ({
+				'15': '3.75rem',
+				'18': '4.5rem',
+				'25': '6.25rem',
+				'26': '6.5rem',
+			}),
+		},
 	},
 	variants: {
 		extend: {},
