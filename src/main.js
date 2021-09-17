d2b26827200a7c8ac7e25bc538a5e79a6cd11a6b
Pawel <Pawel@Tests-MacBook-Air.local>
2021-09-17 14:21:51 +0200

    Mobile menu space

--- src/main.js
+++ src/main.js
@@ -44,6 +44,7 @@ function isInViewport(element) {
 }
 
 document.querySelector('#burger').addEventListener('click', menuVisibility);
+document.querySelector('.button--close').addEventListener('click', menuVisibility);
 window.addEventListener('resize', function () {
     if(window.innerWidth > 768) {
         document.querySelector('body').classList.remove('body-noScroll')
