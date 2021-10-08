9fe6a2ae685ee2928a6bb679bdef2f7ada44f33f
Pawel <Pawel@Tests-MacBook-Air.local>
2021-10-08 09:53:25 +0200

    Fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,45 @@
 const { watch, series, src, dest } = require("gulp");
 var browserSync = require("browser-sync").create();
+const concat = require('gulp-concat');
 var postcss = require("gulp-postcss");
 const imagemin = require("gulp-imagemin");
 const uglify = require('gulp-uglify-es').default;
 
 function cssTask(cb) {
-	return src("./src/*.css")
+	return src("./src/css/*.css")
 		.pipe(postcss())
-		.pipe(dest("./assets/css"))
+		.pipe(concat('style.min.css'))
+		.pipe(dest("./src/css"))
 		.pipe(browserSync.stream());
 	cb();
 }
 
 function scriptsTask(cb) {
-	return src('./src/*.js')
+	return src('./src/js/*.js')
 		.pipe(uglify())
-		.pipe(dest('./assets/js'))
+		.pipe(dest('./dist/js'))
 		.pipe(browserSync.stream());
 	cb();
 }
 
 // Task for minifying images
 function imageminTask(cb) {
-	return src("./assets/images/**/*")
+	return src("./src/assets/images/**/*")
 		.pipe(imagemin())
-		.pipe(dest("./assets/images"));
+		.pipe(dest("./dist/assets/images"));
 	cb();
 }
 
-// function htmlBuild(cb) {
-// 	return src("./*.html")
-// 		.pipe(dest("./dist"))
-// 	cb();
-// }
+function htmlBuild(cb) {
+	return src("./src/*.html")
+		.pipe(dest("./dist"))
+	cb();
+}
 
 function browsersyncServe(cb) {
 	browserSync.init({
 		server: {
-			baseDir: "./",
+			baseDir: "src/",
 		},
 	});
 	cb();
@@ -50,12 +52,12 @@ function browsersyncReload(cb) {
 
 // Watch Files & Reload browser after tasks
 function watchTask() {
-	watch("./**/*.html", browsersyncReload);
-	watch(["./src/*.css"], series(cssTask, browsersyncReload));
-	watch(["./src/*.js"], series(scriptsTask, browsersyncReload));
+	watch("./src/**/*.html", browsersyncReload);
+	watch(["./src/css/*.css"], series(cssTask, browsersyncReload));
+	watch(["./src/js/*.js"], series(scriptsTask, browsersyncReload));
 }
 
-exports.build = series(cssTask, scriptsTask, imageminTask);
-exports.default = series(cssTask, scriptsTask, browsersyncServe, watchTask);
+exports.build = series(cssTask, scriptsTask, imageminTask, htmlBuild);
+exports.default = series(cssTask, scriptsTask, htmlBuild, browsersyncServe, watchTask);
 exports.css = cssTask;
 exports.images = imageminTask;
